1e0c4a22665d67354c9ca71037321cea3d77d60a
현영 <hyun0@mail.ulsan.ac.kr>
2025-11-12 03:13:01 +0900

    feat:6주차_mission_2

--- nongbu/UMC-9th-mission/src/pages/HomePage.tsx
+++ nongbu/UMC-9th-mission/src/pages/HomePage.tsx
@@ -1,18 +1,54 @@
-import useGetLpList from "../hooks/queries/useGetLpList.ts";
-import { useState } from "react";
+import { PAGINATION_ORDER } from "../enums/common.ts";
+import useGetInfiniteLpList from "../hooks/queries/useGetInfiniteLpList.tsx";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import type { Lp, ResponseLpListDto } from "../types/lp.ts";
+import LpCard from "../components/LpCard/LpCard.tsx";
+import LpCardSkeletonList from "../components/LpCard/LpCardSkeletonList.tsx";
 
-const HomePage = () => { // Show usages new *
-  const [search, setSearch]= useState("매튜"); // initialState:
-  const { data, isPending, isError } = useGetLpList({ 
-    search,
+const HomePage = () => { 
+  const [search, setSearch]= useState(""); // initialState:
+  // const { data, isPending, isError } = useGetLpList({ 
+  //   search,
+  //   limit: 50,
+  // });
+  const { 
+    data: lps, 
+    isFetching, 
+    hasNextPage, 
+    isPending, 
+    fetchNextPage, 
+    isError 
+  } = useGetInfiniteLpList(10, search, PAGINATION_ORDER.desc);
+
+  // ref, inView
+  // ref -> 특정한 HTML 요소를 감시할 수 있다.
+  // inView -> 그 요소가 화면에 보이면 true
+
+  const { ref, inView } = useInView ({
+      threshold: 0,
   });
+  useEffect( () => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
 
   return (
-    <div className={"mt-20"}>
+    <div className="container mx-auto px-4 py-6">
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      {data?.data.map((lp) => <h1>{lp.title}</h1>)}
+      <div
+        className={
+          "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        }>
+        {lps?.pages
+          ?.map((page : ResponseLpListDto) => page.data.data)
+          ?.flat()
+          ?.map((lp : Lp) => <LpCard key={lp.id} lp={lp} />)}
+        {isFetching && <LpCardSkeletonList count={20} />}
+      </div>
+      <div ref={ref} className="h-2"></div>
     </div>
   );
 };
-
-export default HomePage; // Show usages new *
+export default HomePage; 
