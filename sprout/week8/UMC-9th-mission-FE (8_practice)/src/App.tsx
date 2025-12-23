b08ceca9ae5c4ae2682ab6229c3b096399921af9
sprout <saldiso0564@naver.com>
2025-12-23 14:08:47 +0900

    FEAT:8-2- useThrottle 활용하여 최적화

--- sprout/week8/UMC-9th-mission-FE (8_practice)/src/App.tsx
+++ sprout/week8/UMC-9th-mission-FE (8_practice)/src/App.tsx
@@ -17,6 +17,7 @@ import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import LpDetailPage from './pages/LpDetailPage';
+import ThrottlePage from './pages/ThrottlePage';
 
 // publicRoutes: 인증 없이 접근 가능한 라우트
 const publicRoutes: RouteObject[] = [
@@ -30,6 +31,7 @@ const publicRoutes: RouteObject[] = [
       { path: 'signup', element: <SignupPage /> },
       { path: 'v1/auth/google/callback', element: <GoogleLoginRedirectPage /> },
       { path: 'lps/:lpId', element: <LpDetailPage /> },
+      { path: '/throttle', element: <ThrottlePage /> },
     ],
   },
 ];
